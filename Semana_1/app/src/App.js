f254514aac2104a501e006cb90d5b6392ce90946
Castillo <s.castillo.laiza@accenture.com>
2023-07-24 23:22:03 -0300

    Se sube implementación de la primera clase, parte practica.

--- Semana_1/app/src/App.js
+++ Semana_1/app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { ejercicio1, ejercicio2, ejercicio3, ejercicio5 } from './practicaUno';
 
 function App() {
+  ejercicio1();
+  ejercicio2();
+  ejercicio3();
+  ejercicio5();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
